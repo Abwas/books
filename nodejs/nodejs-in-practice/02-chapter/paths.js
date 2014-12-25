1ee133e69f87d977656d3d2e5bf86d145d15a910
ericdouglas <eric.eoop@gmail.com>
2014-12-25 15:18:08 -0200

    nodejs in practice -> ch02 - use path.join() to alternative of concatenate strings manually

--- nodejs/nodejs-in-practice/02-chapter/paths.js
+++ nodejs/nodejs-in-practice/02-chapter/paths.js
@@ -1,2 +1,6 @@
+var path = require( 'path' );
+
 console.log( '__dirname: ', __dirname );
 console.log( '__filename: ', __filename );
+
+console.log( 'path.join()', path.join( __dirname, 'some', 'folder' ));
