7a791d5d20a935d460c3517fa5f8810a71afd59d
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-26 11:23:42 -0200

    nodejs in practice -> 02 - console object

---
+++ nodejs/nodejs-in-practice/02-chapter/console.js
@@ -0,0 +1,11 @@
+var name = 'Eric';
+var user = {
+  name : 'Eric'
+};
+
+console.log( 'Hello' );
+console.log( 'Hello %s', name );
+console.log( 'Hello:', name );
+console.log( 'Hello:', user );
+
+console.error( 'Error, bad user:', user );
