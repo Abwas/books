187110d42243b8a3b6c07660d3689dd7a49ce0ed
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-26 11:14:00 -0200

    nodejs in practice -> 02 - process

---
+++ nodejs/nodejs-in-practice/02-chapter/process.js
@@ -0,0 +1,9 @@
+// run with:
+//   cat file.txt | node process.js
+
+process.stdin.resume();
+process.stdin.setEncoding( 'utf8' );
+
+process.stdin.on( 'data', function( text ) {
+  process.stdout.write( text.toUpperCase());
+});
