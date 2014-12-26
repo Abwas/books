2250f95fdf7a73fe6e592d1b8da283e01082aa45
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-26 11:26:31 -0200

    nodejs in practice -> 02 - console object placeholders

---
+++ nodejs/nodejs-in-practice/02-chapter/console-placeholders.js
@@ -0,0 +1,10 @@
+var name = 'Eric';
+var number = 13.31;
+var data = {
+  user : 'Eric'
+};
+
+console.log( 'String %s', name );
+console.log( 'Number %d', number );
+console.log( 'Data: %j', data );
+
