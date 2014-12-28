6c88cc2e534627c3bd1ecdf142998cfddb1c6035
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-28 16:00:35 -0200

    nodejs in practice -> 04 - add events to listener

---
+++ nodejs/nodejs-in-practice/04-chapter/inherit-2.js
@@ -0,0 +1,37 @@
+var util = require( 'util' );
+var events = require( 'events' );
+
+var AudioDevice = {
+  play : function( track ) {
+    // Stub: trigger playback through iTunes, mpg123, etc
+  },
+
+  stop : function() {
+  
+  }
+};
+
+function MusicPlayer() {
+  this.playing = false;
+  events.EventEmitter.call( this );
+}
+
+util.inherits( MusicPlayer, events.EventEmitter );
+
+var musicPlayer = new MusicPlayer();
+
+musicPlayer.on( 'play', function( track ) {
+  this.playing = true;
+  AudioDevice.play( track );
+});
+
+musicPlayer.on( 'stop', function() {
+  this.playing = false;
+  AudioDevice.stop();
+});
+
+musicPlayer.emit( 'play', 'The Roots - The Fire' );
+
+setTimeout( function() {
+  musicPlayer.emit( 'stop' );
+});
