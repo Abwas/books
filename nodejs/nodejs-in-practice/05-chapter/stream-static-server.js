efd5862b54717a0fc29e3ede6027a60f98f2c6ac
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-29 12:07:03 -0200

    nodejs in practice -> 05 - stream static server

---
+++ nodejs/nodejs-in-practice/05-chapter/stream-static-server.js
@@ -0,0 +1,10 @@
+var http = require( 'http' );
+var fs = require( 'fs' );
+
+http
+  .createServer( function( req, res ) {
+    fs
+      .createReadStream( __dirname + '/index.html' )
+      .pipe( res );
+  })
+  .listen( 8000 );
