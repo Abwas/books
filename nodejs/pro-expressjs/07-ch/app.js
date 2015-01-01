03998573f686800f3eeca16e0b689d662a5ff9b0
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-01 15:19:50 -0200

    pro express -> 07 - initial project

---
+++ nodejs/pro-expressjs/07-ch/app.js
@@ -0,0 +1,17 @@
+var express = require( 'express' );
+var path = require( 'path' );
+var favicon = require( 'serve-favicon' );
+var logger = require( 'morgan' );
+var cookieParser = require( 'cookie-parser' );
+var bodyParser = require( 'body-parser' );
+
+var routes = require( './routes/index' );
+
+var app = express();
+
+// View engine setup
+app.set( 'views', path.join( __dirname, 'views' ));
+app.set( 'view engine', 'jade' );
+
+app.use( logger( 'combined' ));
+app.use( favicon( path.join( __dirname, 'public', 'favicon.ico' )));
