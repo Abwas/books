dd9c7f20997fb4e5f18d744d7f4dd46f94c0d9d5
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-02 14:29:57 -0200

    pro express -> 08 - get /send-ok route

--- nodejs/pro-expressjs/08-ch/app.js
+++ nodejs/pro-expressjs/08-ch/app.js
@@ -61,6 +61,14 @@ app.get( '/status', function( req, res ) {
   res.status( 200 ).end();
 });
 
+app.get( '/send-ok', function( req, res ) {
+  res
+    .status( 200 )
+    .send({
+      message : 'Data was submitted successfully'
+    });
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
