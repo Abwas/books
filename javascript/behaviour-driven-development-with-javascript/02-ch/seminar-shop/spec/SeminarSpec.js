da1a1a846ab1cd549084fd957afeefcfe696fb34
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-04 16:59:58 -0200

    bdd js -> 02 - adicionando mais um teste

--- javascript/behaviour-driven-development-with-javascript/02-ch/seminar-shop/spec/SeminarSpec.js
+++ javascript/behaviour-driven-development-with-javascript/02-ch/seminar-shop/spec/SeminarSpec.js
@@ -7,5 +7,10 @@ describe( 'Seminar', function() {
   it( 'shoud have a price', function() {
     var seminar = Seminar.create( 'JavaScript-Basics', 499.99 );
     expect( seminar.netPrice()).toEqual( 499.99 );
-  })
+  });
+
+  it( 'should have a gross price that adds 20% VAT to the net price', function() {
+    var seminar = Seminar.create( 'JavaScript-Basics', 100 );
+    expect( seminar.grossPrice()).toEqual( 120 );
+  });
 });
