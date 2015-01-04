cb0d632d59b6419043535ec00d16337f64ec8c67
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-04 12:11:53 -0200

    bdd js -> 02 - usando expect e toEqual para averiguar se o nome está correto

--- javascript/behaviour-driven-development-with-javascript/02-ch/seminar-shop/spec/SeminarSpec.js
+++ javascript/behaviour-driven-development-with-javascript/02-ch/seminar-shop/spec/SeminarSpec.js
@@ -1,6 +1,6 @@
 describe( 'Seminar', function() {
   it( 'should have a name', function() {
     var seminar = Seminar.create( 'JavaScript-Basics' );
-    seminar.name();
+    expect( seminar.name()).toEqual( 'JavaScript-Basics' );
   });
 });
