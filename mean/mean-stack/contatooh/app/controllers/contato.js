1814a380edb220f44902b1613f2d66c477bd022f
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-18 13:11:56 -0200

    mean stack -> 02 - retorna apenas um contato passando seu id na url

--- mean/mean-stack/contatooh/app/controllers/contato.js
+++ mean/mean-stack/contatooh/app/controllers/contato.js
@@ -24,6 +24,22 @@ module.exports = function() {
     res.json( contatos );
   }
 
+  controller.obtemContato = function( req, res ) {
+
+    var idContato = req.params.id;
+    
+    var contato   = contatos.filter( function( contato ) {
+      return contato._id === parseInt( idContato, 10 );
+    })[ 0 ];
+
+    console.log( contato );
+
+    contato ? 
+      res.json( contato ) :
+      res.status( 404 ).send( 'Contato não encontrado' );
+
+  };
+
   return controller;
 
 };
