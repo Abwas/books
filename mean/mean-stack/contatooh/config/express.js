2a04eb6c8b8f5fb789320185416ddc86310cfbe1
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-18 08:48:11 -0200

    mean stack -> 02 - middleware específico para uma rota

--- mean/mean-stack/contatooh/config/express.js
+++ mean/mean-stack/contatooh/config/express.js
@@ -13,6 +13,18 @@ module.exports = function() {
   app.set( 'view engine', 'ejs' );
   app.set( 'views', './app/views' );
 
+  // middlewares definidos para rotas serão executados
+  // em todos os métodos (GET, POST, PUT, DELETE, etc...)
+  app.use( '/', function( req, res, next ) {
+    console.log( 'middleware A na rota /' );
+    next();
+  });
+
+  app.use( '/', function( req, res, next ) {
+    console.log( 'middleware B na rota /' );
+    next();
+  });
+
   // abaixo da config. do último middleware
   // carrega todos os scripts dentro das pastas seguintes
   load( 'models', { cwd : 'app' })
