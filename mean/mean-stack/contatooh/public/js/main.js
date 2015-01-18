e4d8eca7526627de78574e6857f2f72d34fc6872
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-18 18:21:06 -0200

    mean stack -> 04 - definindo rota #/contatos/:contatoId no client

--- mean/mean-stack/contatooh/public/js/main.js
+++ mean/mean-stack/contatooh/public/js/main.js
@@ -6,6 +6,10 @@ angular
       .when( '/contatos', {
         templateUrl : 'partials/contatos.html',
         controller  : 'ContatosController'
+      })
+      .when( '/contatos/:contatoId', {
+        templateUrl : 'partials/contato.html',
+        controller  : 'ContatoController'
       });
 
   });
