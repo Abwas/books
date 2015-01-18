0a8d37d93f5d4b5b309bf2bd1e560e24823e68d9
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-18 18:01:52 -0200

    mean stack -> 04 - definindo rota #/contatos no client

--- mean/mean-stack/contatooh/public/js/main.js
+++ mean/mean-stack/contatooh/public/js/main.js
@@ -1,1 +1,11 @@
-angular.module( 'contatooh', [ 'ngRoute' ] );
+angular
+  .module( 'contatooh', [ 'ngRoute' ] )
+  .config( function( $routeProvider ) {
+
+    $routeProvider
+      .when( '/contatos', {
+        templateUrl : 'partials/contatos.html',
+        controller  : 'ContatosController'
+      });
+
+  });
