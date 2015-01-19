987e729f01f18ee46bfd3a0976820a6c693a676f
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-19 07:34:44 -0200

    mean stack -> 05 - consumindo dados com $resource

--- mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
+++ mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
@@ -1,16 +1,16 @@
 angular
   .module( 'contatooh' )
-  .controller( 'ContatosController', function( $scope, $http ) {
+  .controller( 'ContatosController', function( $scope, $resource ) {
 
     $scope.contatos = [];
     $scope.filtro = '';
-    $http.get( '/contatos' )
-      .success( function( data, status, headers, config ) {
-        $scope.contatos = data;
-      })
-      .error( function( statusText ) {
-        console.log( 'Não foi possível obter a lista de contatos' );
-        console.log( statusText, status, headers, config );
-      });
+    var Contato = $resource( '/contatos' );
+
+    Contato.query( function( contatos ) {
+      $scope.contatos = contatos;
+    }, function( erro ) {
+      console.log( 'Não foi possível obter a lista de contatos' );
+      console.log( erro );
+    });
 
   });
--- mean/mean-stack/contatooh/public/js/main.js
+++ mean/mean-stack/contatooh/public/js/main.js
@@ -1,5 +1,5 @@
 angular
-  .module( 'contatooh', [ 'ngRoute' ] )
+  .module( 'contatooh', [ 'ngRoute', 'ngResource' ] )
   .config( function( $routeProvider ) {
 
     $routeProvider
