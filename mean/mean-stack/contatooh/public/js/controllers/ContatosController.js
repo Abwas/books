e4b64effc5c16cf504ee5a41bb7ce5f7087eadda
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-19 07:22:48 -0200

    mean stack -> 05 - buscando contatos com $http.get

--- mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
+++ mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
@@ -1,28 +1,20 @@
 angular
   .module( 'contatooh' )
-  .controller( 'ContatosController', function( $scope ) {
+  .controller( 'ContatosController', function( $scope, $http ) {
 
     $scope.total = 0;
     $scope.incrementa = function() {
       $scope.total++;
     };
-    $scope.contatos = [
-      {
-        _id   : 1,
-        nome  : 'Contato Exemplo 1',
-        email : 'cont1@empresa.com.br'
-      },
-      {
-        _id   : 2,
-        nome  : 'Contato Exemplo 2',
-        email : 'cont2@empresa.com.br'
-      },
-      {
-        _id   : 3,
-        nome  : 'Contato Exemplo 3',
-        email : 'cont3@empresa.com.br'
-      }
-    ];
+    $scope.contatos = [];
     $scope.filtro = '';
+    $http.get( '/contatos' )
+      .success( function( data, status, headers, config ) {
+        $scope.contatos = data;
+      })
+      .error( function( statusText ) {
+        console.log( 'Não foi possível obter a lista de contatos' );
+        console.log( statusText, status, headers, config );
+      });
 
   });
