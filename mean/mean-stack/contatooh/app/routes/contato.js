76054536deebc3b4b0c499f7fcb21197ab6b78f3
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-20 07:17:41 -0200

    MEAN Stack -> 05 - refactor das rotas usando app.route()

--- mean/mean-stack/contatooh/app/routes/contato.js
+++ mean/mean-stack/contatooh/app/routes/contato.js
@@ -2,8 +2,14 @@ module.exports = function( app ) {
 
   var controller = app.controllers.contato;
 
-  app.get( '/contatos', controller.listaContatos );
-  app.get( '/contatos/:id', controller.obtemContato );
-  app.delete( '/contatos/:id', controller.removeContato );
+  app
+    .route( '/contatos' )
+    .get( controller.listaTodos )
+    .post( controller.salvaContato );
+
+  app
+    .route( '/contatos/:id' )
+    .get( controller.obtemContato )
+    .delete( controller.removeContato );
 
 };
