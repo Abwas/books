140bf86c83ce9c459b10e422a769e641d366b491
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-20 07:56:24 -0200

    MEAN Stack -> 05 - refactor no $resource.delete() - passando callbacks de sucesso e erro

--- mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
+++ mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
@@ -22,11 +22,9 @@ angular
 
     $scope.remove = function( contato ) {
       
-      var promise = Cont.delete({ id : contato._id }).$promise;
-
-      promise
-        .then( buscaContatos )
-        .catch( function( erro ) {
+      Cont.delete({ id : contato._id },
+        buscaContatos,
+        function( erro ) {
           console.log( 'Não foi possível remover o contato' );
           console.log( erro );
         });
