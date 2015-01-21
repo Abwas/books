2967c5ad63d525247f4cf6342d92f64fa9dd10cd
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-21 06:49:18 -0200

    mean stack -> 5 - template pag. contato

--- mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
+++ mean/mean-stack/contatooh/public/js/controllers/ContatosController.js
@@ -3,17 +3,22 @@ angular
   .controller( 'ContatosController', function( $scope, $resource ) {
 
     $scope.contatos = [];
-    $scope.filtro = '';
-    var Contato = $resource( '/contatos' );
-    var Cont    = $resource( '/contatos/:id' );
+    $scope.filtro   = '';
+    $scope.mensagem = { texto : '' };
+
+    var Contato     = $resource( '/contatos' );
+    var Cont        = $resource( '/contatos/:id' );
 
     function buscaContatos() {
 
       Contato.query( function( contatos ) {
         $scope.contatos = contatos;
+        $scope.mensagem = {};
       }, function( erro ) {
-        console.log( 'Não foi possível obter a lista de contatos' );
         console.log( erro );
+        $scope.mensagem = {
+          texto : 'Não foi possível obter a lista de contatos'
+        };
       });
 
     }
@@ -25,8 +30,10 @@ angular
       Cont.delete({ id : contato._id },
         buscaContatos,
         function( erro ) {
-          console.log( 'Não foi possível remover o contato' );
           console.log( erro );
+          $scope.mensagem = {
+            texto : 'Não foi possível remover o contato'
+          };
         });
 
     };
