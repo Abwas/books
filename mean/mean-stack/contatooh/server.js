c39b3bed94d878484b1324c1c4e7c3620d10be29
ericdouglas <eric.douglas.mail@gmail.com>
2015-03-01 11:46:40 -0300

    mean -> 07 - usando mongoose para conectador nosso banco de dados na aplicação

--- mean/mean-stack/contatooh/server.js
+++ mean/mean-stack/contatooh/server.js
@@ -1,6 +1,8 @@
 var http = require( 'http' );
 var app = require( './config/express' )();
 
+require( './config/database.js' )( 'mongodb://localhost/contatooh' );
+
 http
   .createServer( app )
   .listen( app.get( 'port' ), function() {
