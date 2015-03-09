86df81267f39ab9dc17ea427cc3ba02227d177eb
ericdouglas <eric.douglas.mail@gmail.com>
2015-03-08 21:46:12 -0300

    mean -> 07 - retornando contatos com mongoose

--- mean/mean-stack/contatooh/app/controllers/contato.js
+++ mean/mean-stack/contatooh/app/controllers/contato.js
@@ -1,28 +1,24 @@
 var ID_CONTATO_INC = 3;
-var contatos       = [
-  {
-    _id   : 1,
-    nome  : 'Contato Exemplo 1',
-    email : 'cont1@empresa.com.br'
-  },
-  {
-    _id   : 2,
-    nome  : 'Contato Exemplo 2',
-    email : 'cont2@empresa.com.br'
-  },
-  {
-    _id   : 3,
-    nome  : 'Contato Exemplo 3',
-    email : 'cont3@empresa.com.br'
-  }
-];
 
-module.exports = function() {
+module.exports = function( app ) {
 
+  var Contato = app.models.contato;
   var controller = {};
 
   controller.listaContatos = function( req, res ) {
-    res.json( contatos );
+    Contato
+      .find()
+      .exec()
+      then( function( contatos ) {
+        res
+          .json( contatos );
+      }, function( erro ) {
+        console
+          .error( erro );
+        res
+          .status( 500 )
+          .json( erro );
+      });
   }
 
   controller.obtemContato = function( req, res ) {
