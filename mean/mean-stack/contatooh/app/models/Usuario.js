552adfe39a9cfdbbc65ce9df38257705cbc49c59
ericdouglas <eric.douglas.mail@gmail.com>
2015-04-16 09:38:13 -0300

    MEAN -> 07 - adiciona mongoose plugin findOrCreate

--- mean/mean-stack/contatooh/app/models/Usuario.js
+++ mean/mean-stack/contatooh/app/models/Usuario.js
@@ -1,9 +1,13 @@
-var mongoose = require( 'mongoose' );
+var mongoose     = require( 'mongoose' );
+var findOrCreate = require( 'mongoose-findorcreate' ); 
 
 module.exports = function() {
   var schema = mongoose.Schema({
     login : { type : String, required : true, index : { unique : true }},
     nome : { type : Date, default : Date.now }
   });
+
+  schema.plugin( findOrCreate );
+  
   return mongoose.model( 'Usuario', schema );
 };
