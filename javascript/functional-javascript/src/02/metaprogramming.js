53e00de6a192522b4cea2ac0f74cf384222b63a5
Eric Douglas <eric.douglas.mail@gmail.com>
2015-06-29 08:16:20 -0300

    Functional JS - concepts - metaprogramming example - point 2D and 3D

---
+++ javascript/functional-javascript/src/02/metaprogramming.js
@@ -0,0 +1,19 @@
+// Metaprogramming
+function Point2D( x, y ) {
+  this._x = x;
+  this._y = y;
+}
+
+var p1 = new Point2D( 0, 1 );
+console.log( p1 );
+// {_x: 0, _y: 1}
+
+function Point3D( x, y, z ) {
+  Point2D.call( this, x, y );
+  this._z = z;
+}
+
+var p2 = new Point3D( 10, -1, 100 );
+console.log( p2 );
+// {_x: 10, _y: -1, _z: 100}
+
